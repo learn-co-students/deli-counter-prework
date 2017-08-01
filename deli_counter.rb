a33d41d5e642c6b2092bf33aaf317c616faeb36a
Al Artesona <artesonaa2@gmail.com>
2017-08-01 21:24:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 def line(katz_deli)
-  line_string = "The line is currently: "
+  line_string = "The line is currently:"
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-      line_string <<("#{index + 1}. #{name} ")
+      line_string <<(" #{index + 1}. #{name}")
     end
-    puts line_string.rstrip
+    puts line_string
   end
 end
 
