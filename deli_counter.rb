0b2e4866359a3ad104f0503c98ac0f53fe6bfb86
Robert Peck <rspeckpfs@gmail.com>
2017-08-27 01:01:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    phrase = "The line is currently:"
+    counter = 1
+    for name in array do
+      phrase += " #{counter}. #{name}"
+      counter += 1
+    end
+    puts phrase
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{array.shift}."
+  end
+end
