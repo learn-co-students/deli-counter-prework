2d0be5c40991d9b59a063849b45f5066f9ca1b58
jessebillard <jessebillard@mac.com>
2018-03-21 17:22:08 -0400

    Deli Counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+def line(array)
+   if array.size == 0
+       puts "The line is currently empty."
+   else
+     new_array = []
+     array.each_with_index { |name, i|
+         new_array.push("#{i + 1}. #{name}")
+     }
+     puts "The line is currently: #{new_array.join(" ")}"
+   end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+end
+
+def now_serving(array)
+    if array.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{array[0]}."
+      array.shift
+    end
+end
