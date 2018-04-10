959dc4c406789613c01b3b3936f02166dfd4087b
jarretbryan <jarret.bryan@gmail.com>
2018-04-10 17:47:57 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -39,3 +39,11 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.find_index(name) + 1} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{katz_deli.shift}."
+  end
+end
