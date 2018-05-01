4def2f80981c7da131a2c7e70a4967e25ba1126c
mrstevenaweiss <weiss.steven@gmail.com>
2018-05-01 14:32:08 -0400

    Done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  line_string = ""
+  counter = 1
+
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    for name in array
+      line_string += "#{counter}. #{name} "
+      counter += 1
+    end
+    s = line_string.strip
+    puts "The line is currently: #{s}"
+  end
+end
+
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    serving = array.shift
+    puts "Currently serving #{serving}."
+  end
+end 
