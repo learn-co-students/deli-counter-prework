75b03a8d88f8c3d770ea1debe8f773b904591057
github username <github email address>
2018-05-03 01:22:40 -0700

    first attempt. completed #now_serving and #take_a_number. #Line needs editing

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(array, string)
+  array.push(string)
+  puts "Welcome, #{string}. You are number #{array.length} in line."
+end
+
+def line(array)
+  line = []
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    #code here
+    end
+  end
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
