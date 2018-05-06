037c46d37538a45b9f7dc58b01a1ddba38d59960
Anthony Lam <al3101@nyu.edu>
2018-05-06 16:34:41 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = [] 
+
+def line(katz_deli) 
+  if line.size == 0 
+    return "The line is currently empty"
+  else 
+    sentence = "The line is currently: "
+    counter = 1
+    katz_deli.each do |person| 
+      sentence += "#{counter}. person " 
+      counter += 1 
+    end
+    return sentence 
+  end 
+end 
