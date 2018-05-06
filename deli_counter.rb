2d87098a270e998d4dde8b9a61cefb7eb7467614
Anthony Lam <al3101@nyu.edu>
2018-05-06 17:06:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,30 @@
 katz_deli = [] 
 
 def line(katz_deli) 
-  if line.size == 0 
-    return "The line is currently empty"
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
   else 
-    sentence = "The line is currently: "
+    sentence = "The line is currently:"
     counter = 1
     katz_deli.each do |person| 
-      sentence += "#{counter}. person " 
+      sentence += " #{counter}. #{person}" 
       counter += 1 
     end
-    return sentence 
+    puts sentence 
+  end 
+end 
+
+def take_a_number(katz_deli, name) 
+  katz_deli.push(name) 
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end 
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    person = katz_deli[0] 
+    katz_deli.shift() 
+    puts "Currently serving #{person}."
   end 
 end 
