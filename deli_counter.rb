cc8274b214364ae6b6dae88b888fc61e18b48588
github username <github email address>
2018-05-08 13:20:17 -0400

    Implemented all deli counter methods

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(people_line)
+  if people_line.size == 0
+    message = "The line is currently empty."
+  else
+    message = "The line is currently:"
+    counter = 1
+    people_line.each do |person|
+      message = "#{message} #{counter}. #{person}"
+      counter += 1
+    end
+  end
+  puts message
+end
+
+def take_a_number(people_line, name)
+  people_line.push(name)
+  puts "Welcome, #{name}. You are number #{people_line.size} in line."
+end
+
+def now_serving(people_line)
+  if people_line.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    current = people_line.shift
+    puts "Currently serving #{current}."
+  end
+end
