519585e6d4b671fbf10db2b5046380e887436ef7
mguevara717 <marissa.guevara717@gmail.com>
2018-07-15 23:45:35 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def line(katz_line)
+  position = 1
+  queue = []
+
+  if katz_line.length == 0
+    puts "The line is currently empty."
+
+  else
+      #print "The line is currently: "
+      katz_line.each do |customer|
+      queue.push("#{position}. #{customer}")
+      position += 1
+      end
+        puts "The line is currently: #{queue.join(" ")}"
+  end
+end
+
+def take_a_number(katz_line, customer)
+    katz_line.push(customer)
+    puts "Welcome, #{customer}. You are number #{katz_line.length} in line."
+end
+
+def now_serving(katz_line)
+    if katz_line.empty?
+    puts "There is nobody waiting to be served!"
+
+  else
+    puts "Currently serving #{katz_line.first}."
+    katz_line.shift
+  end
+end
