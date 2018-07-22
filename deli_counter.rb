db531b3b03f1a53f97a2d55892c8bc0c01ef8675
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-22 21:20:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(deli)
+  if deli.length>0
+    result=deli.map.with_index{|x,i| "#{i+1}. #{x}"}.join(" ")
+    puts "The line is currently: #{result}"
+    
+  else  
+    puts "The line is currently empty."
+ end
+end
+  
+def take_a_number(deli,name)  
+  len=deli.length+1
+  puts "Welcome, #{name}. You are number #{len} in line."
+  deli.push(name)
+end
+
+def now_serving(deli)
+  if deli.length>0
+    result=deli.shift
+    puts "Currently serving #{result}."
+  else 
+    puts "There is nobody waiting to be served!"
+  end
+end
