2f0f5b4317bca7e50f5aa86a8a3eae197cb31e86
leannemcabey <leanne.m.cabey@gmail.com>
2018-10-10 19:53:06 -0400

    created methods to pass tests

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length < 1
+    puts "The line is currently empty."
+  else
+    message = ["The line is currently:"]
+    katz_deli.each_with_index{ |name, index|
+      message.push(" #{index + 1}. #{name}")
+    }
+    puts message.join
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length < 1
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
