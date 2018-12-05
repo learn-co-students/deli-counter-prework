2c48f47663f18de7743fba0a952f15cd9622f9c8
cannoli9917 <katie.pennachio@gmail.com>
2018-12-04 21:28:01 -0500

    Deli line in ruby

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(deli_line)
+    message = "The line is currently:"
+     if deli_line.count == 0
+         puts "The line is currently empty."
+     else
+        deli_line.each_with_index do |person, index|
+        message += " #{index + 1}. #{person}"
+        end
+        puts message
+     end
+end
+
+
+
+def take_a_number(deli_line, name)
+    deli_line.push(name)
+    puts "Welcome, #{name}. You are number #{deli_line.count} in line."
+end
+
+
+
+
+def now_serving(deli_line)
+    if deli_line.count == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{deli_line.shift}."
+    end
+end
