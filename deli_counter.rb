c8ce14a044aa64683f54630e137c485ec754c67b
ko kristin <koberlander115@gmail.com>
2019-02-16 20:04:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ katz_deli = []
 def line(array)
   if array.length > 0 
     array.each_with_index do | name, index |
-      index += 1 
+     puts "The line is currently: #{index + 1} #{name}." 
     end
   else
-    "The line is currently empty."
+    puts "The line is currently empty."
   end
       
 end
