43d7070e8930d296c6da74dd5a05c4e0b8a72ca4
ko kristin <koberlander115@gmail.com>
2019-02-16 19:58:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,13 @@
 # Write your code here.
 katz_deli = []
 
-
+def line(array)
+  if array.length > 0 
+    array.each_with_index do | name, index |
+      index += 1 
+    end
+  else
+    "The line is currently empty."
+  end
+      
+end
