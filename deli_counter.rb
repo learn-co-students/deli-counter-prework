645b76602c4a4f086ab4196361f6c078e5540062
ascotttoney <ascotttoney@gmail.com>
2019-04-12 12:01:23 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(deli)
+  if deli == []
+    puts "The line is currently empty."
+  else
+    line = []
+    line << deli.join
+      puts "The line is currently: #{line}"
+    end
+  end
+end
+
+# def take_a_number(deli, name)
+#
+# end
+#
+# def now_serving
+#
+# end
