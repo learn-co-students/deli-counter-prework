7e551dccfa44f752047aad8f749f6aac9c79113e
Amy Cen <amy.cen@blackrock.com>
2019-04-14 06:53:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,12 @@ def line(line)
     puts "The line is currently empty."
   else
     i = 0
-    line_list = ''
+    line_list = "The line is currently:"
     while i < line.length
       line_list << " #{i + 1}. #{line[i]}"
       i += 1
     end
-    puts "The line is currently:#{line_list}"
+    puts "#{line_list}"
   end
 end
 
